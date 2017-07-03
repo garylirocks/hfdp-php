cb8cfe7774c309c8f2fd76af67ad7033abe3c5ba
guisheng.li <gary.li.rocks@gmail.com>
2017-07-03 13:22:44 +1200

    add some comments

--- observer/observerDemo.php
+++ observer/observerDemo.php
@@ -6,10 +6,11 @@ include '../init.php';
 $subject = new MySubject();
 $observer = new MyObserver($subject);
 
+// NOTE this observer has already been registered, it can not be registered again
 $subject->registerObserver($observer);
 
 $subject->setSpeed(10);
-
 $subject->removeObserver($observer);
 
+// NOTE observer already removed, no observer here
 $subject->setSpeed(20);
